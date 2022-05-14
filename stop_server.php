960597086ad1f2bcf27f3db3d7126364c55e3f0d
Elias <16616409+EliasPereirah@users.noreply.github.com>
2022-05-14 10:12:08 -0300

    Update stop_server.php

--- stop_server.php
+++ stop_server.php
@@ -1,12 +1,15 @@
 <?php
-exec("ps -ef | grep php", $output);
-foreach ($output as $valor) {
-    if(preg_match("/php -S localhost:8080/",$valor)){
-        preg_match("/\d+/",$valor,$match);
-        if(!empty($match[0])){
-            $pid = $match[0];
-            exec("kill -9 $pid");
-            echo "Process $pid terminated\r\n";
+if (php_sapi_name() == 'cli') {
+    echo "..........\n";
+    exec("ps -ef | grep php", $output);
+    foreach ($output as $valor) {
+        if(preg_match("/php -S localhost:8080/",$valor)){
+            preg_match("/\d+/",$valor,$match);
+            if(!empty($match[0])){
+                $pid = $match[0];
+                exec("kill -9 $pid");
+                echo "Process $pid terminated\r\n";
+            }
         }
     }
 }
